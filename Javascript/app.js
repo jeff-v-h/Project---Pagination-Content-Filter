8689cd67e0c455a764a08045708b8775bd1e04b2
jeffvhuang <jh0791@gmail.com>
2016-10-23 13:24:09 +1100

    Dynamically added pagination link
    Just 1 added, need to add more and tie to number of students

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,6 +1,7 @@
 //Hide all but first 10 students on list
 var studentList = document.getElementsByClassName('student-list')[0];
 var studentItems = document.querySelectorAll('.student-list .student-item');
+var pageDiv = document.querySelector('.page');
 console.log(studentList);
 
   //hide the students
@@ -18,6 +19,40 @@ var showStudents = function() {
   }
 }
 
+//dynamically create pagination links
+  //append a with href '#' and once has class 'active'
+var createPagination = function() {
+  //creating the div and ul and calling function to create pagination number
+  var paginationDivElement = document.createElement('div');
+  var paginationList = document.createElement('ul');
+  var paginationListItem = createPaginationNumber(1, '#');
+
+  paginationDivElement.className = 'pagination';
+  //appending the elements together to prepare for dynamically adding html
+  paginationDivElement.appendChild(paginationList);
+  paginationList.appendChild(paginationListItem);
+
+  return paginationDivElement;
+}
+
+//create pagination number as a list item with link and number
+var createPaginationNumber = function(number, link) {
+  var paginationListItem = document.createElement('li');
+  var linkElement = document.createElement('a');
+  // paginationLink.className = 'active';
+  linkElement.innerText = number;
+  linkElement.href = link;
+  paginationListItem.appendChild(linkElement);
+  return paginationListItem;
+}
 
 hideStudents();
 showStudents();
+//appending the pagination to the page
+var paginationLinks = createPagination();
+pageDiv.appendChild(paginationLinks);
+
+// calculate number of pages needed and add appropriate number of pages
+  // function to count number of student items
+  // function to divide b 10
+  // for each 10 students, append to a new pagination number
